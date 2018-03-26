7fb39da2d55479f3dc933cbe89bad355aab519fc
znlccy <756311868@qq.com>
2018-03-26 18:29:46 +0800

    添加语言包

---
+++ application/lang/en-us.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Author: Administrator
+ * Date: 2018/3/26 17:48
+ * Version: v-1.0.0
+ * Introduce: Central English Language Package
+ */
+
+return [
+    //Relative to shop
+
+];
+
+?>
---
+++ application/lang/zh-cn.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Author: Administrator
+ * Date: 2018/3/26 18:10
+ * Version: v-1.0.0
+ * Introduce: 核心中文语言包
+ */
+return [
+
+    //商城相关
+    'shop_index'                        => '商城首页',
+    'attention_mall'                    => '关注商城',
+    'concerned_official_wechat'         => '关注官方微信',
+    'mobile_terminal'                   => '手机端',
+    'shop_help_center'                  => '帮助中心',
+
+    //会员相关
+    'member_center'                     => '个人中心',
+    'member_buy_treasure'               => '已买宝贝',
+    'member_address_management'         => '地址管理',
+    'member_baby_collection'            => '我的收藏',
+
+    //商品相关
+    'goods_card'                        => '购物车',
+
+    //首页头部
+    'welcome_to'                        => '欢迎来到',
+    'please_login'                      => '请登录',
+    'free_registration'                 => '注册',
+    'login'                             => '登录',
+    'welcome_back'                      => '欢迎您回来',
+    'login_out'                         => '退出',
+    'logout'                            => '退出登录',
+    'quit_successfully'                 => '退出成功',
+
+    //首页搜索
+    'baby'                              => '宝贝',
+    'search'                            => '搜索',
+    'recent_search'                     => '最近搜索',
+    'empty'                             => '清空',
+    'hot_search'                        => '正在热搜中',
+    'shopping_cart_settlement'          => '购物车结算',
+    'my_mall'                           => '我的商城',
+    'please_input_keywords'             => '请输入关键词',
+
+    //商品分类
+    'all_goods_classification'          => '全部商品分类',
+    'view_all_merchandise_categoriea'   => '查看全部商品分类',
+    'no_goods_classification'           => '当前还没有商品分类',
+
+    //右侧
+    'register'                          => '免费注册',
+    'hello'                             => '你好',
+    'please'                            => '请',
+    'last_login_time'                   => '上次登录时间',
+    'order_center'                      => '订单中心',
+    'user'                              => '用',
+    'name'                              => '户',
+    'back_top'                          => '回到顶部',
+    'my_collection'                     => '我的收藏',
+    'contact_customer_service'          => '联系客服',
+    'mall_code'                         => '商城二维码',
+
+    //底部
+    'certified_guarantee'               => '正品保障',
+    'seven_days'                        => '七天包退',
+    'rave_reviews'                      => '好评如潮',
+    'search'                            => '搜索',
+
+];
+?>
